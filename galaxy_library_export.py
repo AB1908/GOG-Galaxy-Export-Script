b6fbd5473dcf29b554cc6ab9cd9d0fba7717dd54
zooglea <majencio@gmail.com>
2020-04-28 00:48:41 +0200

    Update galaxy_library_export.py
    Added code to pick N random games from owned games list.
    
    Usage: 
    
    galaxy_library_export.py -r <number_of_random_picks>

--- galaxy_library_export.py
+++ galaxy_library_export.py
@@ -2,11 +2,28 @@ import sqlite3
 import json
 import time
 import csv
-
+import random
+import sys, getopt
 
 database_location = "C:\\ProgramData\\GOG.com\\Galaxy\\storage\\galaxy-2.0.db"
 platforms = { "3do": "3DO Interactive", "3ds": "Nintendo 3DS", "aion": "Aion", "aionl": "Aion: Legions of War", "amazon": "Amazon", "amiga": "Amiga", "arc": "ARC", "atari": "Atari", "battlenet": "Battle.net", "bb": "BestBuy", "beamdog": "Beamdog", "bethesda": "Bethesda.net", "blade": "Blade & Soul", "c64": "Commodore 64", "d2d": "Direct2Drive", "dc": "Sega Dreamcast", "discord": "Discord", "dotemu": "DotEmu", "egg": "Newegg", "elites": "Elite Dangerous", "epic": "Epic Games Store", "eso": "ESO", "fanatical": "Fanatical store", "ffxi": "Final Fantasy XI", "ffxiv": "Final Fantasy XIV", "fxstore": "Placeholder", "gamehouse": "GameHouse", "gamesessions": "GameSessions", "gameuk": "Game UK", "generic": "Other", "gg": "GamersGate", "glyph": "Trion World", "gmg": "Green Man Gaming", "gog": "GOG", "gw": "Guild Wars", "gw2": "Guild Wars 2", "humble": "Humble Bundle", "indiegala": "IndieGala", "itch": "Itch.io", "jaguar": "Atari Jaguar", "kartridge": "Kartridge", "lin2": "Lineage 2", "minecraft": "Minecraft", "n64": "Nintendo64", "ncube": "Nintendo GameCube", "nds": "Nintendo DS", "neo": "NeoGeo", "nes": "NES", "ngameboy": "Nintendo Game Boy", "nswitch": "Nintendo Switch", "nuuvem": "Nuuvem", "nwii": "Nintendo Wii", "nwiiu": "Nintendo Wii U", "oculus": "Oculus", "origin": "Origin", "paradox": "Paradox Plaza", "pathofexile": "Path of Exile", "pce": "PC Engine", "playasia": "Play-Asia", "playfire": "Playfire", "ps2": "Sony PlayStation 2", "psn": "PlayStation Network", "psp": "PlayStation Portable", "psvita": "PlayStation Vita", "psx": "Sony PlayStation", "riot": "Riot", "rockstar": "Rockstar Games Launcher", "saturn": "SegaSaturn", "sega32": "Sega 32X", "segacd": "Sega CD", "segag": "Sega Genesis", "sms": "Sega Master System", "snes": "SNES", "stadia": "Google Stadia", "star": "Star Citizen", "steam": "Steam", "test": "Test", "totalwar": "Total War", "twitch": "Twitch", "unknown": "Unknown", "uplay": "Uplay", "vision": "ColecoVision", "wargaming": "Wargaming", "weplay": "WePlay", "winstore": "Windows Store", "xboxog": "Original Xbox games", "xboxone": "Xbox Live", "zx": "Zx Spectrum PC" }
 
+# Read parameters
+arguments = sys.argv
+arguments = arguments[1:]
+
+numberOfPicks = 0
+try:
+  opts, args = getopt.getopt(arguments,"r:",["picks="])
+except getopt.GetoptError:
+	print ("galaxy_library_export.py -r <number_of_random_picks>")
+	sys.exit(2)
+for opt, arg in opts:
+	if opt == '-r':
+		numberOfPicks = arg
+
+#print("N: ", numberOfPicks)
+  
 conn = sqlite3.connect(database_location)
 cursor = conn.cursor()
 
@@ -19,10 +36,10 @@ cursor.execute("""SELECT id FROM GamePieceTypes WHERE type = 'originalTitle'""")
 originalTitleID = cursor.fetchall()[0][0]
 cursor.execute("""SELECT id FROM GamePieceTypes WHERE type = 'title'""")
 titleID = cursor.fetchall()[0][0]
-# Create a view of OwnedGames joined on GamePieces for a full OwnedGames DB
+# Create a view of GameLinks joined on GamePieces for a full GameLinks DB
 owned_games_query = """CREATE TEMP VIEW MasterList AS
-				SELECT GamePieces.releaseKey, GamePieces.gamePieceTypeId, GamePieces.value FROM OwnedGames
-				JOIN GamePieces ON OwnedGames.releaseKey = GamePieces.releaseKey;"""
+				SELECT GamePieces.releaseKey, GamePieces.gamePieceTypeId, GamePieces.value FROM GameLinks
+				JOIN GamePieces ON GameLinks.releaseKey = GamePieces.releaseKey;"""
 # Create view of unique items using details
 unique_owned_games_query = """CREATE TEMP VIEW UniqueMasterList AS
 				SELECT DISTINCT(MasterList.value) AS metadata, MasterCopy.value AS title FROM MasterList, MasterList
@@ -30,16 +47,32 @@ unique_owned_games_query = """CREATE TEMP VIEW UniqueMasterList AS
 				((MasterCopy.gamePieceTypeId = {}) OR (MasterCopy.gamePieceTypeId = {})) AND
 				(MasterCopy.releaseKey = MasterList.releaseKey);""".format(originalMetaID, metaID, originalTitleID, titleID)
 # Display each game and its details along with corresponding release key grouped by details
-aggr_game_data = """SELECT UniqueMasterList.title, GROUP_CONCAT(DISTINCT MasterList.releaseKey), UniqueMasterList.metadata
+aggr_game_data = """CREATE TEMP VIEW FinalList AS
+				SELECT UniqueMasterList.title, GROUP_CONCAT(DISTINCT MasterList.releaseKey), UniqueMasterList.metadata
 				FROM UniqueMasterList, MasterList WHERE UniqueMasterList.metadata = MasterList.value
 				GROUP BY UniqueMasterList.metadata ORDER BY UniqueMasterList.title;"""
+# count distinct games
+number_of_games = """SELECT COUNT(*) FROM FinalList;"""
+				
 cursor.execute(owned_games_query)
 cursor.execute(unique_owned_games_query)
 cursor.execute(aggr_game_data)
+
+# pick random game
+cursor.execute("""SELECT COUNT(*) from FinalList;""")
+numberOfGames = cursor.fetchone()[0]
+print("Number of games: ", numberOfGames)
+
+picks = random.sample(range(0, int(numberOfGames)), int(numberOfPicks))
+#print(picks)
+
+cursor.execute("""SELECT * FROM FinalList""")
+
 with open("gameDB.csv", "w", encoding='utf-8', newline='') as csvfile:
 	fieldnames = ['title', 'platformList', 'developers', 'publishers', 'releaseDate', 'genres', 'themes', 'criticsScore']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
+	gameIndex = 0
 	while True:
 		result = cursor.fetchone()
 		if result:
@@ -49,6 +82,10 @@ with open("gameDB.csv", "w", encoding='utf-8', newline='') as csvfile:
 			row = metadata
 			row['title'] = result[0].split('"')[3]
 			row['title'] = row['title'].replace("\\","")
+			
+			if (gameIndex in picks):
+				print("Random selection [{:d}]: ".format(gameIndex), row['title'])
+
 			row['platformList'] = []
 			if any(platform in releaseKey for platform in platforms for releaseKey in result[1].split(",")):
 				row['platformList'] = set(platforms[platform] for releaseKey in result[1].split(",") for platform in platforms if releaseKey.startswith(platform))
@@ -66,6 +103,8 @@ with open("gameDB.csv", "w", encoding='utf-8', newline='') as csvfile:
 				if type(value) == list or type(value) == set:
 					row[key] = ",".join(value)
 			writer.writerow(row)
+			
+			gameIndex = gameIndex + 1
 		else:
 			break
 cursor.close()
