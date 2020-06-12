25ccd81360844f029a7c61b2220ad1a83151e3e5
Varstahl <Varstahl@users.noreply.github.com>
2020-06-12 14:59:29 +0200

    boolean parameters default checks against `<all> or <parameter>`

--- galaxy_library_export.py
+++ galaxy_library_export.py
@@ -8,6 +8,7 @@ class Arguments():
 	""" argparse wrapper, to reduce code verbosity """
 	__parser = None
 	__args = None
+	__bAll = False  # Extract all fields
 
 	def __init__(self, args, **kwargs):
 		import argparse
@@ -15,6 +16,7 @@ class Arguments():
 		for arg in args:
 			self.__parser.add_argument(*arg[0], **arg[1])
 		self.__args = self.__parser.parse_args()
+		self.__bAll = getattr(self.__args, 'all')
 
 	def help(self):
 		self.__parser.print_help()
@@ -26,7 +28,8 @@ class Arguments():
 		return False
 
 	def __getattr__(self, name):
-		return getattr(self.__args, name)
+		ret = getattr(self.__args, name)
+		return (self.__bAll or ret) if isinstance(ret, bool) else ret
 
 def extractData(args):
 	database_location = "C:\\ProgramData\\GOG.com\\Galaxy\\storage\\galaxy-2.0.db"
@@ -113,7 +116,7 @@ def extractData(args):
 		og_resultGroupBy = ['MasterDB.platformList']
 
 		# Create parameterised filtered view of owned games using multiple joins
-		if args.all or args.summary:
+		if args.summary:
 			prepare(
 				'summary',
 				{'summary': True},
@@ -123,22 +126,22 @@ def extractData(args):
 				'MasterDB.summary'
 			)
 
-		if args.all or args.platforms:
+		if args.platforms:
 			prepare(
 				'platforms',
 				{'platformList': True},
 			)
 
-		if args.all or args.criticsScore or args.developers or args.genres or args.publishers or args.releaseDate or args.themes:
+		if args.criticsScore or args.developers or args.genres or args.publishers or args.releaseDate or args.themes:
 			prepare(
 				'metadata',
 				{
-					'criticsScore': args.all or args.criticsScore,
-					'developers': args.all or args.developers,
-					'genres': args.all or args.genres,
-					'publishers': args.all or args.publishers,
-					'releaseDate': args.all or args.releaseDate,
-					'themes': args.all or args.themes,
+					'criticsScore': args.criticsScore,
+					'developers': args.developers,
+					'genres': args.genres,
+					'publishers': args.publishers,
+					'releaseDate': args.releaseDate,
+					'themes': args.themes,
 				},
 				'METADATA.value AS metadata',
 				'MasterList AS METADATA',
@@ -146,7 +149,7 @@ def extractData(args):
 				'MasterDB.metadata'
 			)
 
-		if args.all or args.playtime:
+		if args.playtime:
 			prepare(
 				'playtime',
 				{'gameMins': True},
@@ -156,13 +159,13 @@ def extractData(args):
 				'sum(MasterDB.time)'
 			)
 
-		if args.all or args.imageBackground or args.imageSquare or args.imageVertical:
+		if args.imageBackground or args.imageSquare or args.imageVertical:
 			prepare(
 				'images',
 				{
-					'backgroundImage': args.all or args.imageBackground,
-					'squareIcon': args.all or args.imageSquare,
-					'verticalCover': args.all or args.imageVertical
+					'backgroundImage': args.imageBackground,
+					'squareIcon': args.imageSquare,
+					'verticalCover': args.imageVertical
 				},
 				'IMAGES.value AS images',
 				'MasterList AS IMAGES',
@@ -187,73 +190,76 @@ def extractData(args):
 			writer.writeheader()
 			while True:
 				result = cursor.fetchone()
-				if not result:
-					break
+				if not result: break  # Break on end
+				if not result[positions['playtime']]: continue  # Skip unfinished imports
 
-				# JSON string needs to be converted to dict
-				# For json.load() to work correctly, all double quotes must be correctly escaped
-				row = {'title': jls('title', True)}
+				try:
+					# JSON string needs to be converted to dict
+					# For json.load() to work correctly, all double quotes must be correctly escaped
+					row = {'title': jls('title', True)}
 
-				# Playtime
-				if args.all or args.playtime:
-					row['gameMins'] = result[positions['playtime']]
+					# Playtime
+					if args.playtime:
+						row['gameMins'] = result[positions['playtime']]
 
-				# Summaries
-				if args.all or args.summary:
-					row['summary'] = jls('summary', True)
+					# Summaries
+					if args.summary:
+						row['summary'] = jls('summary', True)
 
-				# Platforms
-				if args.all or args.platforms:
-					rkeys = result[positions['releaseKey']].split(',')
-					if any(platform in releaseKey for platform in platforms for releaseKey in rkeys):
-						row['platformList'] = set(platforms[platform] for releaseKey in rkeys for platform in platforms if releaseKey.startswith(platform))
-					else:
-						row['platformList'] = ["Placeholder"]
+					# Platforms
+					if args.platforms:
+						rkeys = result[positions['releaseKey']].split(',')
+						if any(platform in releaseKey for platform in platforms for releaseKey in rkeys):
+							row['platformList'] = set(platforms[platform] for releaseKey in rkeys for platform in platforms if releaseKey.startswith(platform))
+						else:
+							row['platformList'] = ["Placeholder"]
 
-				# Various metadata
-				if args.all or args.criticsScore or args.developers or args.genres or args.publishers or args.releaseDate or args.themes:
-					metadata = jls('metadata')
+					# Various metadata
+					if args.criticsScore or args.developers or args.genres or args.publishers or args.releaseDate or args.themes:
+						metadata = jls('metadata')
 
-					if args.all or args.criticsScore:
-						try:
-							row['criticsScore'] = round(metadata['criticsScore'])
-						except:
-							row['criticsScore'] = metadata['criticsScore']
+						if args.criticsScore:
+							try:
+								row['criticsScore'] = round(metadata['criticsScore'])
+							except:
+								row['criticsScore'] = metadata['criticsScore']
 
-					if args.all or args.developers:
-						row['developers'] = metadata['developers']
+						if args.developers:
+							row['developers'] = metadata['developers']
 
-					if args.all or args.genres:
-						row['genres'] = metadata['genres']
+						if args.genres:
+							row['genres'] = metadata['genres']
 
-					if args.all or args.publishers:
-						row['publishers'] = metadata['publishers']
+						if args.publishers:
+							row['publishers'] = metadata['publishers']
 
-					if args.all or args.releaseDate:
-						try:
-							row['releaseDate'] = time.strftime("%Y-%m-%d", time.localtime(metadata['releaseDate']))
-						except:
-							row['releaseDate'] = metadata['releaseDate']
+						if args.releaseDate:
+							try:
+								row['releaseDate'] = time.strftime("%Y-%m-%d", time.localtime(metadata['releaseDate']))
+							except:
+								row['releaseDate'] = metadata['releaseDate']
 
-					if args.all or args.themes:
-						row['themes'] = metadata['themes']
+						if args.themes:
+							row['themes'] = metadata['themes']
 
-				# Original images
-				if args.all or args.imageBackground or args.imageSquare or args.imageVertical:
-					images = jls('images')
-					if args.all or args.imageBackground:
-						row['backgroundImage'] = images['background'] or ''
-					if args.all or args.imageSquare:
-						row['squareIcon'] = images['squareIcon'] or ''
-					if args.all or args.imageVertical:
-						row['verticalCover'] = images['verticalCover'] or ''
+					# Original images
+					if args.imageBackground or args.imageSquare or args.imageVertical:
+						images = jls('images')
+						if args.imageBackground:
+							row['backgroundImage'] = images['background'] or ''
+						if args.imageSquare:
+							row['squareIcon'] = images['squareIcon'] or ''
+						if args.imageVertical:
+							row['verticalCover'] = images['verticalCover'] or ''
 
-				# CSV listification
-				for key, value in row.items():
-					if type(value) == list or type(value) == set:
-						row[key] = ",".join(value)
+					# CSV listification
+					for key, value in row.items():
+						if type(value) == list or type(value) == set:
+							row[key] = ",".join(value)
 
-				writer.writerow(row)
+					writer.writerow(row)
+				except:
+					print('Parsing failed on: {}'.format(result))
 
 if __name__ == "__main__":
 	def ba(variableName, description, defaultValue=False):
