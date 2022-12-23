c83345d44db298c7765e559def1c462385c544b2
Marc Bäckman <marc.backman@edu.lapinamk.fi>
2022-12-24 00:15:53 +0200

    Removed the sorted games list
    Now it only prints a numbered list of all the games in alphabetical order into a text file.

--- helper_scripts/print_gameDB.py
+++ helper_scripts/print_gameDB.py
@@ -1,14 +1,13 @@
+
+# PRINTS NUMBERED LIST OF GAMES FROM CSV FILE TO TEXT FILE
+
 import csv
-import re
 
 # String that contains all the games.
 my_games = ""
 
 # Index for the game.
 i = 0
-
-dates = []
-games = []
 
 # Open csv file to reader
 with open('../gameDB.csv', 'r', encoding='utf-8') as csv_file:
@@ -17,38 +16,14 @@ with open('../gameDB.csv', 'r', encoding='utf-8') as csv_file:
     # The name of the game is on the first part of the row
     for row in reader:
         row_string = ' '.join([str(elem) for elem in row])
-        new_list_from_row = re.split(" |\t", row_string)
-        for item in new_list_from_row:
-            if len(item) == 10 and "-" in item:
-                numbers = item.replace("-", "")
-                if numbers.isnumeric():
-                    dates.append(numbers)
-                    print(numbers)
-                    break
 
         name = row[0].split('\t')
 
         # Add the index to the game.
         if i > 0:
             my_games += str(i) + ". " + name[0] + "\n"
-        games.append(name[0])
         print(name[0])
         i += 1
-
-games.pop(0)
-
-print(f"dates length: {len(dates)}")
-print(f"games length: {len(games)}")
-
-zipped_pairs = zip(dates, games)
-sorted_games = [x for _, x in sorted(zipped_pairs)]
-
-i = 1
-my_games_sorted = ""
-for item in sorted_games:
-    my_games_sorted += str(i) + ". " + item + "\n"
-    i += 1
-
 
 # Write the string to file.
 with open('my_games.txt', 'w', encoding='utf-8') as games_file:
@@ -56,8 +31,3 @@ with open('my_games.txt', 'w', encoding='utf-8') as games_file:
 
 # Close the file.
 games_file.close()
-
-with open('my_games_sorted.txt', 'w', encoding='utf-8') as games_file_sorted:
-    games_file_sorted.write(my_games_sorted)
-
-games_file_sorted.close()
